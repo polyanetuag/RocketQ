cdda2e1670985ee12851a2298627e64011fa7c0a
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-26 14:46:39 -0300

    Chamando as rotas de acordo com a página

--- src/route.js
+++ src/route.js
@@ -2,10 +2,10 @@ const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}))
 
+route.get('/room', (req, res) => res.render("room"))
 
 //formato que o formulario de dentro do modal tem que passar a informação
 route.get('/room/:room/:question/:action', QuestionController.index )
