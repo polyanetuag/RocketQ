f74fbbee9f20de8a40d73d94e519af71c44751f0
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-26 15:49:32 -0300

    Modificando rotas para criapção de salas

--- src/route.js
+++ src/route.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
+
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}))
 
-route.get('/room', (req, res) => res.render("room"))
+route.get('/room/:room', (req, res) => res.render("room"))
 
 //formato que o formulario de dentro do modal tem que passar a informação
-route.get('/room/:room/:question/:action', QuestionController.index )
-
+route.get('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 
 module.exports = route
