f67a32c8da74de9c3d62ef3e39f46a0590d3f5db
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-27 18:02:13 -0300

    Criando banco de dados

--- src/db/config.js
+++ src/db/config.js
@@ -1,9 +1,8 @@
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
-module.exports = () => {
+module.exports = () => 
   open({
     filename: './src/db/roquetq.sqlite',
-    driver: sqlite3.Database
-  })
-}
+    driver: sqlite3.Database,
+  });
