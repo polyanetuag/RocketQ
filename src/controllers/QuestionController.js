c74fe3fc56d87d442e15091f2d804aebfbc5ceab
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-27 19:18:49 -0300

    Integrando perguntas no banco de dados dinamicamente

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,8 +1,29 @@
+const Database = require('../db/config')
+
 module.exports = {
+  
   index(req, res) {
     const roomId = req.params.room
     const questionId = req.params.question
     const action = req.params.action
     const password = req.body.password
+  },
+
+  async create(req, res) {
+    const db = await Database()
+    const question = req.body.question
+    const roomId = req.params.room
+
+    await db.run(`INSERT INTO questions (
+      title,
+      room,
+      read
+    )VALUES(
+      "${question}",
+      ${roomId},
+      0
+    )`)
+
+    res.redirect(`/room/${roomId}`)
   }
 }
--- src/route.js
+++ src/route.js
@@ -7,11 +7,12 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
+route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
 
 //formato que o formulario de dentro do modal tem que passar a informação
+route.post('/question/create/:room', QuestionController.create)
 route.post('/question/:room/:question/:action', QuestionController.index)
-route.post('/create-room', RoomController.create)
 
 
 module.exports = route
