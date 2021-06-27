f2b358f40591033615e668519e21e3156e52e6d0
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-27 19:51:52 -0300

    Ações de excluir e marcar como lida

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -2,11 +2,30 @@ const Database = require('../db/config')
 
 module.exports = {
   
-  index(req, res) {
+  async index(req, res) {
+    const db = await Database()
     const roomId = req.params.room
     const questionId = req.params.question
     const action = req.params.action
     const password = req.body.password
+
+    /*Verificar se a senha está correta */
+    const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+    if(verifyRoom.pass == password ) {
+      if(action == "delete") {
+
+        await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+
+      } else if (action == "check") {
+
+        await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+
+      }
+      res.redirect(`/room/${roomId}`)
+    } else {
+      res.render('passincorrect', {roomId: roomId})
+    }
+
   },
 
   async create(req, res) {
