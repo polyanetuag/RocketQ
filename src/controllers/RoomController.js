0810115ef63d34cae3375663a64803cc4b9cf0cd
Polyane Tuag <pollyportugues@hotmail.com>
2021-06-27 18:30:09 -0300

    Inserindo número da sala dinamicamente

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -34,5 +34,10 @@ module.exports = {
     await db.close()
 
     res.redirect(`/room/${roomId}`)
+  }, 
+  
+  open(req, res) {
+    const roomId = req.params.room
+    res.render("room", {roomId: roomId})
   }
 }
